be9f82c6fde3e62430efeb0474f0212c941c79af
nico-develop <contact@nico-develop.fr>
2017-10-03 10:16:58 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import * as Hapi from 'hapi';
+
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+server.route({
+    method: 'GET',
+    path:'/hello/{name}',
+    handler: function (request, reply) {
+
+        return reply('hello ' + request.params['name']);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path:'/',
+    handler: function (request, reply) {
+
+        return reply('Nice');
+    }
+});
+
+server.start((err) => {
+    if (err) { throw err; }
+    console.log('Server running at:', server.info.uri);
+});
